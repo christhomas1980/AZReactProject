0e32746ea2d1019dad10145c8254c628a04e04b5
christhomas1980 <christhomas1980@gmail.com>
2025-09-22 10:27:27 -0500

    Add files via upload

---
+++ AzureCloudApi/Models/CloudTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AzureCloudApi.Models;
+
+public partial class CloudTable
+{
+    public int Empid { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Amount { get; set; }
+
+    public string Email { get; set; }
+}
