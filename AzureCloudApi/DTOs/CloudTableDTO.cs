00fe70a4ac7e5f774e7d900d0ff148bf599f419e
christhomas1980 <christhomas1980@gmail.com>
2025-09-22 10:26:50 -0500

    Add files via upload

---
+++ AzureCloudApi/DTOs/CloudTableDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CloudAppApi.DTO;
+
+public partial class CloudTableDTO
+{
+    public int Empid { get; }
+
+    public string Name { get; set; } = null!;
+
+    public int Amount { get; set; }
+
+    public string Email { get; set; }
+}
